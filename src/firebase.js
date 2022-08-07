fc7c208fb0e7ac973f2297bd7e5c8af08ba45aab
Samet.POLAT <netflix.uyelik84@gmail.com>
2022-08-08 01:59:32 +0300

    amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC7tMLZKM8g1qb6bw95URDZPPJHOz8rLHI",
+    authDomain: "clone-a1f1c.firebaseapp.com",
+    projectId: "clone-a1f1c",
+    storageBucket: "clone-a1f1c.appspot.com",
+    messagingSenderId: "928762771287",
+    appId: "1:928762771287:web:51b4f68f525b7bba07f012",
+    measurementId: "G-YQ2M8MSTBF"
+  };
