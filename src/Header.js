87b76ed716ef0987219b5ca7aabcdacd98453272
Samet.POLAT <netflix.uyelik84@gmail.com>
2022-08-08 02:51:57 +0300

    devam

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
-
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 function Header() {
   return (
     <div className="header">
@@ -12,7 +13,7 @@ function Header() {
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
-        {/*Logo*/}
+        <SearchIcon className="header_searchIcon" />
       </div>
 
       <div className="header__nav">
@@ -27,6 +28,11 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
